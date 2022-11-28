0a828760d3456660f72d33da1cf4cfb4ee30c184
Mego Elhawary <ahmedelhawary@seekasia.com>
2022-11-28 12:41:17 +0800

    - update format

--- src/api/path1Handler.ts
+++ src/api/path1Handler.ts
@@ -1,28 +1,35 @@
-import { Request, Response } from 'express';
-import { initPinoLogger } from '../framework/logger';
+import { Request, Response } from "express";
+import { initPinoLogger } from "../framework/logger";
 
 const logger = initPinoLogger("path1Handler");
 
 export const path1Handler = async (req: Request, res: Response) => {
-  const path1HandlerRequest = {}; // case the request details here to strong type
+  const path1HandlerRequest = {}; // caste the request details here to strong type
   logger.info({
-    message: 'processing request',
+    message: "processing request",
     path1HandlerRequest,
     ...req.logTags,
   });
 
-  /***
-   * Add logic here
-   */
-  await Promise.all([]);  // TODO: Replace with logic async calls
+  const path1HandlerResponse = await processPath1Request(
+    path1HandlerRequest
+  );
 
-
-  const path1HandlerResponse = 'Hello World !'  // set the final reply
   logger.info({
-    message: 'request completed',
+    message: "request completed",
     path1HandlerResponse,
     ...req.logTags,
   });
   res.status(200).json(path1HandlerResponse);
 };
 
+export const processPath1Request = async (
+  _path1Request: unknown
+): Promise<unknown> => {
+  /***
+   * Add logic here
+   */
+  await Promise.all([]); // TODO: Replace with logic async calls
+  const path1Response = "Hello World !";
+  return path1Response;
+};
