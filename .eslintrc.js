511aa192ce151262943ae19e1a73a1f4d40d12a7
Mego Elhawary <ahmedelhawary@seekasia.com>
2022-12-11 15:40:59 +0800

    - eslint  the config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true
     },
-    "extends": [
-        "eslint:all", // recommended
-        "plugin:@typescript-eslint/recommended",
+    extends: [
+        'eslint:all',
+        'plugin:@typescript-eslint/recommended'
     ],
-    "overrides": [],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest'
     },
-    "plugins": [
-        "@typescript-eslint",
-        "sort-keys-fix"
+    plugins: [
+        '@typescript-eslint',
+        'sort-keys-fix'
     ],
-    "rules": {
-        "quotes": [2, "single"],
-        "quote-props": [2, "as-needed"],
-        "sort-keys-fix/sort-keys-fix": "warn",
+    rules: {
+        'quote-props': [
+            'error',
+            'as-needed'
+        ],
+        quotes: [
+            'error',
+            'single'
+        ],
+        'sort-keys-fix/sort-keys-fix': 'warn'
     }
 };
