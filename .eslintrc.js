fba551a59619653c01e81e80058cdcba89c5f0f6
Mego Elhawary <ahmedelhawary@seekasia.com>
2022-12-11 16:31:11 +0800

    link lint with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,20 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es2021: true
-    },
-    extends: [
-        'eslint:all',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    overrides: [],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest'
-    },
-    plugins: [
-        '@typescript-eslint',
-        'sort-keys-fix'
-    ],
-    rules: {
-        'quote-props': [
-            'error',
-            'as-needed'
-        ],
-        quotes: [
-            'error',
-            'single'
-        ],
-        'sort-keys-fix/sort-keys-fix': 'warn'
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['eslint:all', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['@typescript-eslint', 'sort-keys-fix', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'quote-props': ['error', 'as-needed'],
+    quotes: ['error', 'single'],
+    'sort-keys-fix/sort-keys-fix': 'warn',
+  },
 };
--- src/framework/logger.ts
+++ src/framework/logger.ts
@@ -1,14 +1,14 @@
+import { config } from '../../config';
 import pino from 'pino';
 
-import {config} from '../../config';
-
-export const initPinoLogger = (name: string): pino.Logger => pino({
+export const initPinoLogger = (name: string): pino.Logger =>
+  pino({
     base: {},
     formatters: {
-        level: (level) => ({level})
+      level: (level) => ({ level }),
     },
     level: config.logLevel,
     name,
     redact: ['req.headers.authorization'],
-    timestamp: pino.stdTimeFunctions.isoTime
-});
+    timestamp: pino.stdTimeFunctions.isoTime,
+  });
