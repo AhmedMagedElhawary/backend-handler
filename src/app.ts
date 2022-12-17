c23662fba0556a92e83e66091c661c1f45a3bfd4
Mego Elhawary <ahmedelhawary@seekasia.com>
2022-12-18 07:30:21 +0800

    enable swagger

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,18 @@
-/*
- * Import swaggerUi from 'swagger-ui-express';
- * import YAML from 'yamljs';
- */
 import { healthCheckHandler } from './api/healthCheck';
 import { smokeTestHandler } from './api/smokeTest';
 import { errorHandler } from './middleware/errorHandler';
 import appRouter from './router/appRouter';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 const app = express();
 
-/*
- * TODO : add support for swagger
- * app.use(
- *   '/api-docs',
- *   swaggerUi.serve,
- *   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
- *   swaggerUi.setup(YAML.load(`${process.cwd()}/src/swagger/swagger-config.yml`)),
- * );
- */
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(YAML.load(`${process.cwd()}/src/swagger/swagger-config.yml`)),
+);
 
 app.get('/health', healthCheckHandler);
 app.get('/smoke', smokeTestHandler);
